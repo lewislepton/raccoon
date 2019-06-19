fe956d1a16fab51c4d5ab07b4d2a28e84795ccd4
Lewis Lepton <lewisleptondev@gmail.com>
2019-06-20 00:03:57 +0100

    added up to episode 134

--- Sources/raccoon/Scene.hx
+++ Sources/raccoon/Scene.hx
@@ -1,11 +1,14 @@
 package raccoon;
 
+import haxe.ds.ArraySort;
 import kha.Canvas;
 import raccoon.Object;
 
 class Scene {
   public var countEntities(get, null):Int;
   private var _entities:Array<Object>;
+
+  private var _depth:Bool;
 
   public function new(){
     _entities = new Array<Object>();
@@ -16,6 +19,8 @@ class Scene {
   }
 
   public function render(canvas:Canvas){
+    if (_depth) depth(_entities);
+
     for (entity in _entities) entity.render(canvas);
   }
 
@@ -36,5 +41,22 @@ class Scene {
     entity.active = false;
     _entities.remove(entity);
     entity = null;
+  }
+
+  function depth(entities:Array<Object>){
+    if (entities.length == 0) return;
+    ArraySort.sort(entities, function(ent1:Object, ent2:Object){
+      if (ent1.position.y + ent1.height < ent2.position.y + ent2.height){
+        return -1;
+      } else if (ent1.position.y == ent2.position.y){
+        return 0;
+      } else {
+        return 1;
+      }
+    });
+  }
+
+  public function sort(?value:Bool = false):Bool {
+    return _depth = value;
   }
 }
